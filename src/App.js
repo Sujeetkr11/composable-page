b1aeb0d50546603276dbdb71675b6e4f7560dd11
devappdigital <devapp.digital@gmail.com>
2024-10-05 07:09:58 -0500

    initial code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+//declarative
+function ItemList() {
+  return (
+    <ul>
+      <li>I am learning reactJS</li>
+      <li>this is my first project</li>
+      <li>So far loved it</li>
+    </ul>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello, world!</h1>
+      <ItemList />  
+      <ItemList />
     </div>
   );
 }
